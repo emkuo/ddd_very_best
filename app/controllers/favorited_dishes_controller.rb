db2ee1b7dcf5677c71e35b368b17d4d94066cacb
anonymous anonymous <emilykuo511@gmail.com>
2021-11-15 19:04:49 +0000

    Remove controller comments

--- app/controllers/favorited_dishes_controller.rb
+++ app/controllers/favorited_dishes_controller.rb
@@ -1,27 +1,22 @@
 class FavoritedDishesController < ApplicationController
   before_action :set_favorited_dish, only: %i[show edit update destroy]
 
-  # GET /favorited_dishes
   def index
     @q = FavoritedDish.ransack(params[:q])
     @favorited_dishes = @q.result(distinct: true).includes(:bookmarks,
                                                            :users).page(params[:page]).per(10)
   end
 
-  # GET /favorited_dishes/1
   def show
     @bookmark = Bookmark.new
   end
 
-  # GET /favorited_dishes/new
   def new
     @favorited_dish = FavoritedDish.new
   end
 
-  # GET /favorited_dishes/1/edit
   def edit; end
 
-  # POST /favorited_dishes
   def create
     @favorited_dish = FavoritedDish.new(favorited_dish_params)
 
@@ -33,7 +28,6 @@ class FavoritedDishesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /favorited_dishes/1
   def update
     if @favorited_dish.update(favorited_dish_params)
       redirect_to @favorited_dish,
@@ -43,7 +37,6 @@ class FavoritedDishesController < ApplicationController
     end
   end
 
-  # DELETE /favorited_dishes/1
   def destroy
     @favorited_dish.destroy
     redirect_to favorited_dishes_url,
@@ -52,12 +45,10 @@ class FavoritedDishesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_favorited_dish
     @favorited_dish = FavoritedDish.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def favorited_dish_params
     params.require(:favorited_dish).permit(:name, :cuisine, :venues_id,
                                            :user_id)
